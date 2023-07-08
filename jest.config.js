618164bb2c07d4f49101850f06b153ab166f304d
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2023-07-08 11:26:06 -0500

    Add coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['clover', 'json', 'lcov', ['text', {skipFull: true}]],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: -100,
+    },
+  },
 };
+
+module.exports = config
